677ca6cc3ef59bcaf86068f2bda6fa5361cfb00c
Lior Bashan <liorbashanx@gmail.com>
2020-04-24 20:30:33 +0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import Router from "vue-router";
+// import HomePage from '@/home/HomePage';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    // {
+    //   path: "/",
+    //   name: "Home",
+    //   components: {
+    //     default: HomePage,
+    //     sidebar: SidebarStandard,
+    //   },
+    // },
+  ],
+});
